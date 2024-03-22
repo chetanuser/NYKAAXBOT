897a7bbbfad5bccb59e5781ba07fb7ff61ac2ab4
chetan anand <121672141+chetanuser@users.noreply.github.com>
2024-03-22 20:46:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -94,10 +94,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/982b01ba53c3d69b0d0ce.jpg"
+    "START_IMG_URL", "https://graph.org/file/f586172fe40a0b5d0b0df.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/file/dec61e858d57c14343455.jpg"
+    "PING_IMG_URL", "https://graph.org/file/f586172fe40a0b5d0b0df.jpg"
 )
 PLAYLIST_IMG_URL = "https://telegra.ph/file/7795e58425337d0455e95.jpg"
 STATS_IMG_URL = "https://graph.org/file/136c57e473c33a0c62152.jpg"
